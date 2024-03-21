204e0f5a2790eccffc6c31f1a6b3393f0bf66673
quguiyou <quguiyou1076@qq.com>
2024-03-21 22:31:27 +0800

    3.4-3.6

---
+++ chapter03/3.4.cpp
@@ -0,0 +1,20 @@
+// 一个线程安全栈的概要类定义
+#include <exception>
+#include <memory>  // For std::shared_ptr<>
+
+struct empty_stack : std::exception {
+  const char *what() const throw();
+};
+
+template <typename T>
+class threadsafe_stack {
+ public:
+  threadsafe_stack();
+  threadsafe_stack(const threadsafe_stack &);
+  threadsafe_stack &operator=(const threadsafe_stack &) = delete;
+
+  void push(T new_value);
+  std::shared_ptr<T> pop();
+  void pop(T &value);
+  bool empty() const;
+};
