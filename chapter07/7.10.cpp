d2416770ec04d636624c5d2132e55b926d24b08c
quguiyou <quguiyou1076@qq.com>
2024-04-12 21:44:24 +0800

    7.10

---
+++ chapter07/7.10.cpp
@@ -0,0 +1,39 @@
+// 代码清单7.10　采用std::experimental::atomic_shared_ptr<>实现的栈容器
+#include <atomic>
+#include <memory>
+
+template <typename T>
+class lock_free_stack {
+ private:
+  struct node {
+    std::shared_ptr<T> data;
+    std::experimental::atomic_shared_ptr<node> next;
+    node(T const &data) : data(std::make_shared<T>(data_)) {}
+  };
+  std::experimental::atomic_shared_ptr<node> head;
+
+ public:
+  void push(T const &data) {
+    std::shared_ptr<node> const new_node = std::make_shared<node>(data);
+    new_node->next = head.load();
+    while (!head.compare_exchange_weak(new_node->next, new_node))
+      ;
+  }
+
+  std::shared_ptr<T> pop() {
+    std::shared_ptr<node> old_head = head.load();
+    while (old_head &&
+           !head.compare_exchange_weak(old_head, old_head->next.load()))
+      ;
+    if (old_head) {
+      old_head->next = std::shared_ptr<node>();
+      return old_head->data;
+    }
+    return std::shared_ptr<T>();
+  }
+
+  ~lock_free_stack() {
+    while (pop())
+      ;
+  }
+};
