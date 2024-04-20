cf25687bce1ef8109e14c3944fd2ad2ce439d352
quguiyou <quguiyou1076@qq.com>
2024-04-20 09:08:47 +0800

    9.1

---
+++ chapter09/9.1.cpp
@@ -0,0 +1,53 @@
+// 代码清单9.1　最简易可行的线程池
+#include <atomic>
+#include <functional>
+#include <thread>
+#include <vector>
+
+template <typename T>
+class threadsafe_queue {
+ public:
+  void push(T);
+  void pop(T &);
+  bool try_pop(T &);
+};
+
+class join_threads {
+ public:
+  join_threads(std::vector<std::thread>);
+};
+
+class thread_pool {
+  std::atomic_bool done;
+  threadsafe_queue<std::function<void()>> work_queue;
+  std::vector<std::thread> threads;
+  join_threads joiner;
+  void worker_thread() {
+    while (!done) {
+      std::function<void()> task;
+      if (work_queue.try_pop(task)) {
+        task();
+      } else {
+        std::this_thread::yield();
+      }
+    }
+  }
+
+ public:
+  thread_pool() : done(false), joiner(threads) {
+    unsigned const thread_count = std::thread::hardware_concurrency();
+    try {
+      for (unsigned i = 0; i < thread_count; ++i) {
+        threads.push_back(std::thread(&thread_pool::worker_thread, this));
+      }
+    } catch (...) {
+      done = true;
+      throw;
+    }
+  }
+  ~thread_pool() { done = true; }
+  template <typename FunctionType>
+  void submit(FunctionType f) {
+    work_queue.push(std::function<void()>(f));
+  }
+};
