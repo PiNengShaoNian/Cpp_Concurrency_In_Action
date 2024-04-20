74a12df9672eef8cdf585f999886aa4d55912407
quguiyou <quguiyou1076@qq.com>
2024-04-20 15:09:09 +0800

    9.6

---
+++ chapter09/9.6.cpp
@@ -0,0 +1,58 @@
+// 代码清单9.6　一个线程池，其中的任务队列以线程局部变量的形式存在
+#include <atomic>
+#include <future>
+#include <memory>
+#include <queue>
+
+template <typename T>
+class threadsafe_queue {
+ public:
+  void push(T);
+  void pop(T &);
+  bool try_pop(T &);
+};
+
+struct function_wrapper {
+  void operator()();
+};
+
+class thread_pool {
+  std::atomic<bool> done;
+  threadsafe_queue<function_wrapper> pool_work_queue;
+  typedef std::queue<function_wrapper> local_queue_type;
+  static thread_local std::unique_ptr<local_queue_type> local_work_queue;
+  void worker_thread() {
+    local_work_queue.reset(new local_queue_type);
+    while (!done) {
+      run_pending_task();
+    }
+  }
+
+ public:
+  template <typename FunctionType>
+  std::future<typename std::result_of<FunctionType()>::type> submit(
+      FunctionType f) {
+    typedef typename std::result_of<FunctionType()>::type result_type;
+    std::packaged_task<result_type()> task(f);
+    std::future<result_type> res(task.get_future());
+    if (local_work_queue) {
+      local_work_queue->push(std::move(task));
+    } else {
+      pool_work_queue.push(std::move(task));
+    }
+    return res;
+  }
+
+  void run_pending_task() {
+    function_wrapper task;
+    if (local_work_queue && !local_work_queue->empty()) {
+      task = std::move(local_work_queue->front());
+      local_work_queue->pop();
+      task();
+    } else if (pool_work_queue.try_pop(task)) {
+      task();
+    } else {
+      std::this_thread::yield();
+    }
+  }
+};
